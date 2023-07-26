41b400f50cbd2144c3bba223ba3bb6149a7789da
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2023-07-26 02:53:25 +0000

    Finished chapter 1 and 2

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,19 +5,28 @@ use std::cmp::Ordering;
 fn main() {
     println!("Input a number");
 
-    let mut guess = String::new(); // This is a mutable variable that is empty for now
     let correct = rand::thread_rng().gen_range(1..=100);
     println!("Pssst! The correct answer is {correct}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Something went wrong somehow lmao"); // note how on the .expect you need a semi
-    
-    let guess: u8 = guess.trim().parse().expect("Please type a number 1-100!");
-    println!("Don't tell me you seriously guessed {guess}...");
-    match guess.cmp(&correct) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        let mut guess = String::new(); // This is a mutable variable that is empty for now
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Something went wrong somehow lmao"); // note how on the .expect you need a semi
+        
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Don't tell me you seriously guessed {guess}...");
+        match guess.cmp(&correct) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
     }
 }
